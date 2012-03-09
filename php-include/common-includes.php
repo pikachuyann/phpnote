212ab971a93500da9fe0370223a65e44149b88f2
Yann Duplouy <pika@pikachuyann.fr>
2012-03-09 14:24:21 +0100

    hack sale pour la connexion mysql

---
+++ php-include/common-includes.php
@@ -0,0 +1,7 @@
+<?php
+	include("../php-include/mysql.php");
+	include("../php-include/style-v1.php");
+
+	/* Gestion des identifications */
+
+?>
---
+++ php-include/style-v1.php
@@ -0,0 +1,23 @@
+<?php
+	function haut_de_page($userinfo,$titre="") {
+		// Userinfo contiendra les données de l'utilisateur dont en aprticulier ses droits (ça pourrait être utile...)
+		if ($titre != "") { $titre = " :: "; }
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title><?= $titre ?>phpNote</title>
+</head>
+<body>
+<?php
+	}
+
+	function bas_de_page($userinfo) {
+		// Userinfo n'est pas forcément utile ?
+?>
+</body>
+</html>
+<?php
+	}
+?>
