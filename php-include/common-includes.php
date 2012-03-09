ae97d884dfaf622e50ffa91ff91a3b89057658ee
Yann Duplouy <pika@pikachuyann.fr>
2012-03-09 15:26:02 +0100

    Ajout de la gestion des SID

--- php-include/common-includes.php
+++ php-include/common-includes.php
@@ -1,7 +1,20 @@
 <?php
+	date_default_timezone_set("Europe/Paris");
+	$time=time();
+
 	include("../php-include/mysql.php");
 	include("../php-include/style-v1.php");
 
 	/* Gestion des identifications */
-
+	if (!isset($_COOKIE["sid"])) {
+	}
+	else {
+		$sid=mysql_real_escape($_COOKIE["sid"]);
+		/* Il faudrait trouver un meilleur moyen pour traiter l'expiration des SID */
+		$reply=mysql_query("SELECT * FROM sid, adherents WHERE sid.sid='$sid' AND sid.expiration<$time AND sid.numcbde=adherents.numcbde");
+		while ($answer=mysql_fetch_assoc($reply)) { // Le sid étant unique
+			// On a plus ou moins l'userinfo dans la réponse...
+			$userinfo=$answer;			
+		}
+	}	
 ?>
--- php-include/mysql.php
+++ php-include/mysql.php
@@ -1,4 +1,14 @@
 <?php
-	$sqlPointer=mysql_connect("localhost","phpnote","hVW2np7QJwdxhhYd")k;
-	mysql_select_db("phpnote",$sqlPointer);
+	// Bonjour, ceci est un hack relativement sale pour forcer la déconnexion au serveur MySQL à la fin de chaque script
+	class MySql_connexion {
+		function __construct() {
+			$this->sqlPointer=mysql_connect("localhost","phpnote","hVW2np7QJwdxhhYd")k;
+			mysql_select_db("phpnote",$this->sqlPointer);
+		}
+		function __destruct() {
+			mysql_close($this->sqlPointer);
+		}
+	}
+	$sql_class = new MySql_connexion();
+	$sqlPointer = $sql_class->sqlPointer;
 ?>
