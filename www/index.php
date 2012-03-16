072d92b169bed3217f6b146818a81327c3019f5f
Yann Duplouy <pika@pikachuyann.fr>
2012-03-16 13:48:25 +0100

    Rajout de texte index.php

--- www/index.php
+++ www/index.php
@@ -1,4 +1,6 @@
 <?php
 	include("../php-include/common-includes.php");
+	haut_de_page($userinfo);
 ?>
-Test
+<p>Bienvenue sur phpNote, la <i>note Kfet</i> en php en cours de d&eacute;veloppement!</p>
+<?php	bas_de_page($userinfo);	?>
