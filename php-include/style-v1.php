17b685eed4608f8c54d0e947470899ee3614a6cc
Yann Duplouy <pika@pikachuyann.fr>
2012-03-16 14:54:44 +0100

    So fixing a bug

--- php-include/style-v1.php
+++ php-include/style-v1.php
@@ -7,7 +7,7 @@
 
 	function haut_de_page($userinfo,$titre="") {
 		// Userinfo contiendra les données de l'utilisateur dont en aprticulier ses droits (ça pourrait être utile...)
-		if ($titre != "") { $titre = " :: "; }
+		if ($titre != "") { $titre = "$titre :: "; }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
