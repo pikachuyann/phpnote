b4008e7a271870454b9232081c740f800457e34e
Yann Duplouy <pika@pikachuyann.fr>
2012-03-16 16:12:39 +0100

    Fix XHTML5

--- php-include/login.php
+++ php-include/login.php
@@ -1,27 +1,37 @@
 <?php
+
+function do_login($post)
+{
+  $req = "SELECT numcbde FROM Adherents 
+          WHERE pseudo='".protect($post['user'])."' 
+            AND motdepasse='".mash($post['pwd'])."'
+            AND valide=1;";
+  $reponse = mysql_query($req);
+  // ToDo
+
+}
+
 function login_page($source, $msg)	{
-	$userinfo["numcbde"]=-1;
-	haut_de_page($userinfo,"Identification n&eacute;cessaire");
+  $userinfo["numcbde"]=-1;
+  haut_de_page($userinfo,"Identification n&eacute;cessaire");
 ?>
-Bienvenue sur <strong>phpnote</strong>.<BR />
-<?= $msg ?>
+<p>Bienvenue sur <strong>phpnote</strong>.</p>
+<p><?= $msg ?></p>
 <form method="post" action="<?= $source ?>">
-<p><table>
+<table>
 <tr><td>Nom d'utilisateur:</td><td><input type="text" name="user"/></td></tr>
 <tr><td>Mot de passe:</td><td><input type="password" name="pwd"/></td></tr>
 <? // Pour la gestion des levels ?>
 <tr><td>Droits:</td>
-  <td><input type="radio" name="level" value="2" name="level2" id="level2" checked="checked"/><label for="level2">Tout mes droits</label></td></tr>
+  <td><input type="radio" name="level" value="2" id="level2" checked="checked"/><label for="level2">Tout mes droits</label></td></tr>
 <tr><td></td>
-  <td><input type="radio" name="level" value="1" name="level1" id="level1"/><label for="level1">Droits note seulement</label></td></tr>
+  <td><input type="radio" name="level" value="1" id="level1"/><label for="level1">Droits note seulement</label></td></tr>
 <tr><td></td>
   <td><input type="submit" value="Envoyer"/></td></tr>
-</table></p>
+</table>
 </form>
 
 <?= bas_de_page(NULL) ?>
-</body>
-</html>
 <?php
 }
 ?>
