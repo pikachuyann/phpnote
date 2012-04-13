0c3ecc52bddf830c368bd043887e64c3cd451887
Yann Duplouy <pika@pikachuyann.fr>
2012-04-13 15:33:45 +0200

    Page de préinscription, première version

--- www/preinscription.php
+++ www/preinscription.php
@@ -4,19 +4,32 @@
 
 	if ($userinfo["numcbde"]==-1) {
 
-		if (isset($_POST["nom"])) { $nom=$_POST["nom"]; } else { $nom=""; }
-		if (isset($_POST["prenom"])) { $prenom=$_POST["prenom"]; } else { $prenom=""; }
-		if (isset($_POST["sect"])) { $sect=$_POST["sect"]; } else { $sect=""; }
-		if (isset($_POST["email"])) { $email=$_POST["email"]; } else { $email=""; }
-		if (isset($_POST["tel"])) { $tel=$_POST["tel"]; } else { $tel=""; }
+		if (isset($_POST["nom"])) { $nom=trim($_POST["nom"]); } else { $nom=""; }
+		if (isset($_POST["prenom"])) { $prenom=trim($_POST["prenom"]); } else { $prenom=""; }
+		if (isset($_POST["sect"])) { $sect=trim($_POST["sect"]); } else { $sect=""; }
+		if (isset($_POST["email"])) { $email=trim($_POST["email"]); } else { $email=""; }
+		if (isset($_POST["tel"])) { $tel=trim($_POST["tel"]); } else { $tel=""; }
+		if (isset($_POST["sexe"])) { $sexe=trim($_POST["sexe"]); echo $sexe; } else { $sexe=""; }
 
 		if (isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["sexe"]) && isset($_POST["sect"]) && isset($_POST["email"]) && isset($_POST["tel"]) && isset($_POST["mdpA"]) && isset($_POST["mdpB"])) {
-			if ($_POST["sexe"] == "M" || $_POST["sexe"] == "F") { $errorMsg="Vous &ecirc;tes de sexe inconnu? Soyez honn&ecirc;tes..."; }
+			if (!($sexe == "M" || $sexe == "F")) { $errorMsg="Vous &ecirc;tes de sexe inconnu? Soyez honn&ecirc;tes..."; }
 			elseif (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$",$_POST["email"])) {
 				$errorMsg="L'adresse e-mail entr&eacute;e ne correspond pas &agrave; une adresse email valide...";
 			}
 			elseif ($_POST["mdpA"] == $_POST["mdpB"]) {
-			
+				$verif_a="SELECT * FROM adherents WHERE email='".mysql_real_escape_string($_POST["email"])."' OR numero_tel='".mysql_real_escape_string($_POST["tel"])."'";
+				$verif_aR=mysql_query($verif_a);
+				$verif_aR=mysql_fetch_assoc($verif_aR);
+				if ($verif_aR["email"] || $verif_aR["tel"]) {
+					$errorMsg="L'email ou le t&eauc;tel&eaucte;phone enregistr&eacute; a d&eacute;j&agrave; &eacute;t&eacute; utilis&eacute; !";
+				}
+				else {
+					$requete="INSERT INTO adherents(nom,prenom,sexe,section,email,numero_tel,motdepasse,preinscription) "
+					." VALUES('".mysql_real_escape_string($nom)."','".mysql_real_escape_string($prenom)."','$sexe','".mysql_real_escape_string($sect)."','".mysql_real_escape_string($email)."','".mysql_real_escape_string($tel)."','".mysql_real_escape_string($_POST["mdpA"])."',1)";
+					mysql_query($requete);
+					$errorMsg="<strong>Votre pr&eacute;inscription a &eacute;t&eacute; effectu&eacute;e avec succ&egrave;s !</strong>";
+					// Let's get the administrative thing working
+				}
 			}
 			else {
 				$errorMsg="Veuillez entrer des mots de passe identiques...";
@@ -43,7 +56,7 @@
 <?php	if (isset($errorMsg)) {	?><tr class='preinscr_errMsg'><td colspan='2'><?= $errorMsg ?></td></tr><?php } ?>
 <tr><th> Nom : </th><td> <input type='text' name='nom' value='<?= $nom ?>' /> </td></tr>
 <tr><th> Pr&eacute;nom : </th><td> <input type='text' name='prenom' value='<?= $prenom ?>' /> </td></tr>
-<tr><th> Sexe : </th><td> <select name='sexe'><option value='M'> Masculin </option><option value='F'> F&eacute;minin </option></select> </td></tr>
+<tr><th> Sexe : </th><td> <select name='sexe'><option value='M'> Masculin </option><option value='F' selected='selected'> F&eacute;minin </option></select> </td></tr>
 <tr><th> Section : </th><td> <input type='text' name='sect' value='<?= $sect ?>' /> </td></tr>
 <tr><th> Courriel : </th><td> <input type='text' name='email' value='<?= $email ?>' /> </td></tr>
 <tr><th> T&eacute;l&eacute;phone : </th><td> <input type='text' name='tel' value='<?= $tel ?>' /> </td></tr>
