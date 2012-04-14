5ec8e17e80dd358418f6f96697e6ffd3162ad067
Jonathan Lajus <skippy@pikachuyann.fr>
2012-04-14 18:04:52 +0200

    Voici la modification à répercuter sur les autres pages pour éviter des bugs.

--- www/index.php
+++ www/index.php
@@ -1,16 +1,15 @@
 <?php
-	include("../php-include/common-includes.php");
-if (isset($_POST['user']) & isset($_POST['pwd']))
-  {
-    do_login($_POST);
-  }
-        if ($userinfo["numcbde"] == -1) {	
-		login_page("index.php","Bienvenue sur <strong>phpNote</strong>, veuillez vous identifier pour acc&eacute;der aux diff&eacute;rentes pages de gestion de la note. Si vous ne disposez pas de compte, vous pouvez vous pr&eacute;inscrire.");
-	}
-	else {
-	haut_de_page($userinfo);
+include("../php-include/common-includes.php");
+
+if (!su(INTRANET)) {	
+  login_page("index.php","Bienvenue sur <strong>phpNote</strong>, veuillez vous identifier pour acc&eacute;der aux diff&eacute;rentes pages de gestion de la note. Si vous ne disposez pas de compte, vous pouvez vous pr&eacute;inscrire.");
+}
+else {
+  haut_de_page($userinfo);
 ?>
 <p>Bienvenue sur phpNote, la <i>note Kfet</i> en php en cours de d&eacute;veloppement !</p>
 <p>Vous pouvez utiliser le menu ci-dessus pour naviguer dans la note Kfet !</p>
-<?php	bas_de_page($userinfo);	
-	}	?>
+<?php	
+  bas_de_page($userinfo);	
+}
+?>
