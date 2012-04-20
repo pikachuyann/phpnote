931dcc4bd545bd3e84df29bf9ebb77beb3047baf
Yann Duplouy <pika@pikachuyann.fr>
2012-04-20 16:31:54 +0200

    deadline: mis à jour

--- www/deadlines.php
+++ www/deadlines.php
@@ -11,9 +11,34 @@
 </ul>
 <p>Les dates de la phpNote :</p>
 <ul style='margin-top:0px;'>
-<li> <strong>Version &beta; 0.1:</strong> avant le 20 avril 2012:
+<li> <strong>Version &beta; 0.1:</strong> aujourd'hui 20 avril 2012 vers 19h00:
 <ul style='margin-top:0px;'>
-<li> Gestion des pr&eacute;inscription et des adh&eacute;rents, de la modification des droits </li>
+<li> Gestion des pr&eacute;inscription et des adh&eacute;rents<del>, de la modification des droits</del> </li>
+</ul>
+<li> <strong>Version &beta; 0.2:</strong>
+<ul style='margin-top:0px;'>
+<li> Gestion des boutons </li>
+<li> La Note en elle-m&ecirc;me (transferts, utilisations de boutons pour un adh&eacute;rent pr&eacute;cis), en version largement am&eacute;liorable (type version &alpha;)</li>
+</ul>
+<li> … </li>
+<li> <strong>Version finale:</Strong> avant le 18 mai 2012
+<ul style='margin-top:0px;'>
+<li> Gestion des transactions: 
+<ul style='margin-top:0px;'>
+<li> Rajout ou retrait d'argent sur le compte </li>
+<li> Utilisation des boutons </li>
+<li> Transactions entre adh&eacute;rents </li>
+<li> Recherche efficace d'un adh&eacute;rent, en prenant en compte l'historique des pseudos </li>
+</ul>
+<li> Gestion des adh&eacute;rents :
+<ul style='margin-top:0px;'>
+<li> Modification des noms de note (pseudos), des droits, et des informations sur les comptes </li>
+</ul>
+<li> Gestion des activit&eacute;s :
+<ul style='margin-top:0px;'>
+<li> Cr&eacute;ation et modification des activit&eacute;s, de leurs lieux, de leurs dates, et de leurs responsables (a choisir parmi les adh&eacute;rents) </li>
+<li> Disponibilit&eacute; d'un fichier externe (<i>xml</i>) permettant de pouvoir voir les activit&eacute;s de l'ext&eacute;rieur.
+</ul>
 </ul>
 </ul>
 <?php	
