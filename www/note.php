0b01f0878b3af9df695b4ad080499cd3e64b78e3
Yann Duplouy <pika@pikachuyann.fr>
2012-05-18 23:29:00 +0200

    Style, passage 2

--- www/note.php
+++ www/note.php
@@ -20,11 +20,11 @@ else
 <td></td><td></td>
 </tr>
 <tr>
-<td><div id="boite-adh"><select size="4"></select></div> <!-- un tableau ou des options --></td>
-<td><div id="affiche_selection"></div> <!-- un tableau ou des options -->
+<td valign='top'><div id="boite-adh"><select size="8"></select></div> <!-- un tableau ou des options --></td>
+<td valign='top'><div id="affiche_selection"></div> <!-- un tableau ou des options -->
 </td>
-<td><?= gen_categories() ?></td>
-<td>
+<td valign='top'><?= gen_categories() ?></td>
+<td valign='top'>
 <div id="boite-boutons"></div> <!-- des boutons -->
 </td>
 <tr>
@@ -38,11 +38,15 @@ else
 </tr>
 </table>
 
+<div>
 <select id="historique" size="10" onScroll="historique_scroll(this, -1)" multiple="multiple">
 </select>
-<button onClick="historique_reset(-1)">Rafra&icirc;chir;</button>
-<button onClick="historique_validate()">Valider tout</button>
-<button onClick="historique_unvalidate()">D&eacute;valider tout</button>
+<span id="boutons_historique">
+<button onClick="historique_reset(-1)">Rafra&icirc;chir</button><BR />
+<button onClick="historique_validate()">Valider les transactions s&eacute;l&eacute;ctionn&eacute;es</button><BR />
+<button onClick="historique_unvalidate()">D&eacute;valider les transactions s&eacute;l&eacute;ctionn&eacute;es</button><BR />
+</span>
+</div>
 <script type="text/javascript" src="js-include/note.js"></script>
 <script type="text/javascript" src="js-include/rtc.js"></script>
 <script type="text/javascript" src="js-include/historique.js"></script>
