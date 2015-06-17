4e666c7fc82079eba872e42b836f27211ae20394
Yann Duplouy <yann@duplouy.name>
2015-06-17 10:19:49 +0200

    Anonymisation du champ Base de Données

--- php-include/sample_mysql.php
+++ php-include/sample_mysql.php
@@ -1,11 +1,11 @@
 <?php
-	// A COPIER EN mysql.php APRES avoir modifié les champs SERVEUR USERNAME et MOTDEPASSE
+	// A COPIER EN mysql.php APRES avoir modifié les champs SERVEUR USERNAME MOTDEPASSE et BASEDEDONNEES
 
 	// Bonjour, ceci est un hack relativement sale pour forcer la déconnexion au serveur MySQL à la fin de chaque script
 	class MySql_connexion {
 		function __construct() {
 			$this->sqlPointer=mysql_connect("SERVEUR","USERNAME","MOTDEPASSE");
-			mysql_select_db("phpnote",$this->sqlPointer);
+			mysql_select_db("BASEDEDONNEES",$this->sqlPointer);
 		}
 		function __destruct() {
 			mysql_close($this->sqlPointer);
